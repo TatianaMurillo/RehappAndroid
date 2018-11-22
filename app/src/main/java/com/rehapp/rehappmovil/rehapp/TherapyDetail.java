e0e75450ac6233cced8129a20589b5f17391f504
TatianaMurillo <yulymurillo87@gmail.com>
2018-11-22 00:20:52 -0500

    tool bar en detalle de terapia

--- app/src/main/java/com/rehapp/rehappmovil/rehapp/TherapyDetail.java
+++ app/src/main/java/com/rehapp/rehappmovil/rehapp/TherapyDetail.java
@@ -3,12 +3,16 @@ package com.rehapp.rehappmovil.rehapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 public class TherapyDetail extends AppCompatActivity {
 private int therapySelected;
 private TextView tvTherapySequence;
+private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,9 @@ private TextView tvTherapySequence;
 
         tvTherapySequence.setText("Terapia # " + (therapySelected+1));
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
     }
@@ -33,5 +40,36 @@ private TextView tvTherapySequence;
     public void watchExercises(View view) {
         Intent intent = new Intent(TherapyDetail.this,TherapyExercises.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu,menu);
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch(item.getItemId())
+        {
+            case R.id.back:
+                intent = new Intent(this, TherapyDetail.class);
+                startActivity(intent);
+                break;
+            case R.id.future:
+                 intent = new Intent(this, ExerciseVideo.class);
+                startActivity(intent);
+                break;
+            case R.id.home:
+                intent = new Intent(this, TherapyDetail.class);
+                startActivity(intent);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/rehapp/rehappmovil/rehapp/TherapyExercises.java
+++ app/src/main/java/com/rehapp/rehappmovil/rehapp/TherapyExercises.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -23,6 +24,15 @@ public class TherapyExercises extends AppCompatActivity {
         itemExample.add("Ejercicio 2");
         itemExample.add("Ejercicio 3");
         itemExample.add("Ejercicio 4");
+
+        lvExercises = findViewById(R.id.lvExercises);
+
+
+        ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, itemExample);
+        // Set The Adapter
+        lvExercises.setAdapter(arrayAdapter);
+
 
 
         lvExercises.setOnItemClickListener(new AdapterView.OnItemClickListener() {
